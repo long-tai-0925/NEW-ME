9f03c6ec5bb370f464c56dcd1e48cb9f6e0015ec
Jiang <me@longtai.org>
2026-04-30 16:14:26 +0800

    Update schema-data.js

--- public/javascript/schema-data.js
+++ public/javascript/schema-data.js
@@ -1,4 +1,22 @@
-const personSchema = ;
+const personSchema = {
+      "@context": "https://schema.org",
+      "@type": "Person",
+      "name": "LongTai Jiang",
+      "alternateName": "Jiang",
+      "url": "https://longtai.org",
+      "image": "https://www.gravatar.com/avatar/64a851c812177321ca40d671ad3c014a",
+      "description": "國立台中科技大學學生，資訊社群活躍成員，參與過 COSCUP 與 SITCON。",
+      "jobTitle": "Student Developer",
+      "affiliation": {
+        "@type": "Organization",
+        "name": "國立台中科技大學"
+      },
+      "sameAs": [
+        "https://github.com/long-tai-0925",
+        "https://instagram.com/jiang.0925",
+        "https://t.me/lcngtai"
+      ]
+    };
 
 const script = document.createElement('script');
 script.type = 'application/ld+json';
